f0eda2906cf69c4b8b45df18965c033ad5fe23e0
amazingfrancoisdm <francoisdufourmartel@gmail.com>
2023-05-13 02:17:55 -0400

    Woohoo j'ai commit

--- yt-mp3.py
+++ yt-mp3.py
@@ -1,1 +1,14 @@
-print("hello")
+from pytube import YouTube
+import os
+  
+link = YouTube(str(input("Enter the URL of the video you want to download:")))
+
+video = link.streams.filter(only_audio=True).first()
+  
+out_video = video.download(output_path=".")
+  
+base, ext = os.path.splitext(out_video)
+song = base + '.mp3'
+  
+print(link.title + " has been successfully downloaded.")
+
